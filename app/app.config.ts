a7ee0b6f4731441f1864fb3a587b10a5d7da359c
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-18 20:56:42 +0600

    Create app.config.ts

---
+++ app/app.config.ts
@@ -0,0 +1,23 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      // neutral: "neutral",
+      neutral: "neutral",
+    },
+    // button: {
+    //   slots: {
+    //     base: ["cursor-pointer"],
+    //   },
+    // },
+    // dropdownMenu: {
+    //   slots: {
+    //     base: ["cursor-pointer"],
+    //   },
+    // },
+    // selectMenu: {
+    //   slots: {
+    //     base: ["cursor-pointer"],
+    //   },
+    // },pWNkartcWob7bKdh
+  },
+});
