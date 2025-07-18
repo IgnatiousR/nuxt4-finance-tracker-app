8f50d6d7e2a29d73dcf9f54f5a18c9d62e042112
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-18 20:56:48 +0600

    Create AppFooter.vue

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="border-t border-neutral-700">
+    <div class="p-5 flex-center">
+      {{ currentYear }} &nbsp;<UIcon name="tdesign:copyright" />&nbsp; APP. All
+      Rights Reserved
+    </div>
+  </footer>
+</template>
+
+<script setup>
+const currentYear = new Date().getFullYear();
+</script>
